d0ecf1989b070b679f21f29a6de65d285c8dcea3
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-10-14 09:11:37 -0300

    addProgressInHeader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,34 @@
-import type { AppProps } from 'next/app';
+import React from 'react';
 
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
+
+import '~/styles/nprogress.css';
 import ResetStyle from '~/styles/reset';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const handleStart = () => {
+      NProgress.start();
+    };
+    const handleStop = () => {
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleStop);
+    router.events.on('routeChangeError', handleStop);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleStop);
+      router.events.off('routeChangeError', handleStop);
+    };
+  }, [router]);
+
   return (
     <>
       <Component {...pageProps} />
